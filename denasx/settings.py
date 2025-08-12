eecda18da85cfef9d718c3d4da008594d48ed187
Andyrei02 <acenusa58@gmail.com>
2025-08-12 20:55:03 +0300

    add debug for test in deploy

--- denasx/settings.py
+++ denasx/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "jk5CIN8PQOkaJ0XAs5DaQcZcIdZGGsguqgZt2Csq4B-TLiun0jDTNnouxesUxAjvTuo"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["denasx-production.up.railway.app"]
 
