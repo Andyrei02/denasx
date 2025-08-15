0ac135713b261ad04850e676a0ee215351d22ca8
Andyrei02 <acenusa58@gmail.com>
2025-08-15 19:15:45 +0300

    update license and add license page

--- denasx/urls.py
+++ denasx/urls.py
@@ -19,12 +19,14 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 from content import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='home'),
+    path('license/', TemplateView.as_view(template_name="license.html"), name="license_page"),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
